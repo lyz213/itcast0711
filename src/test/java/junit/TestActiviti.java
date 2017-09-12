673c633790b79e1f16f56761b345252ae543bf85
lyz213 <liyongzhen_it@163.com>
2017-09-12 11:35:10 +0800

    Update TestActiviti.java
    新增222

--- src/test/java/junit/TestActiviti.java
+++ src/test/java/junit/TestActiviti.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class TestActiviti {
 
-	/**使用代码创建工作流需要的23张表111*/
+	/**使用代码创建工作流需要的23张表111222*/
 	@Test
 	public void createTable(){
 		ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
