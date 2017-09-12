602fbba301b231b1f15759d506850802dcb12f53
lyz213 <liyongzhen_it@163.com>
2017-09-12 11:06:45 +0800

    删除111

--- src/test/java/junit/TestActiviti.java
+++ src/test/java/junit/TestActiviti.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class TestActiviti {
 
-	/**使用代码创建工作流需要的23张表1111*/
+	/**使用代码创建工作流需要的23张表*/
 	@Test
 	public void createTable(){
 		ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
